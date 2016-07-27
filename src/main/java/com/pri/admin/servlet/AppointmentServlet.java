65b0e7872d6379260c232199436a5dfad528904d
primedari <priyanka.medari@gmail.com>
2016-07-26 20:00:58 -0400

    Update AppointmentServlet.java

--- src/main/java/com/pri/admin/servlet/AppointmentServlet.java
+++ src/main/java/com/pri/admin/servlet/AppointmentServlet.java
@@ -1,13 +1,11 @@
 package com.pri.admin.servlet;
 
-import java.io.IOException
-;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,11 +13,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import com.pri.dao.PetDao;
 import com.pri.servlet.util.Pet;
-//import com.pri.servlet.util.VisitDao;
 
+             /* Appointment Servlet*/
+             
 @WebServlet (urlPatterns = {"/Appointment"})
 public class AppointmentServlet extends HttpServlet {
 	
@@ -30,56 +28,38 @@ public class AppointmentServlet extends HttpServlet {
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		
-
 		String uri = request.getRequestURI();
-		if(uri.endsWith("Appointment"))
+		if(uri.endsWith("Appointment"))     // URI check is performed
 		{
-		
-			
-			
-			Connection con = (Connection) getServletContext().getAttribute("DBConnection");
+			Connection con = (Connection) getServletContext().getAttribute("DBConnection"); // Database Connection
 			PreparedStatement ps = null;
 			try {
-				
 				 ArrayList<Pet> aPet= new ArrayList<Pet>();
-					
-					ps=con.prepareStatement("select * from Pet ");
-				
-	            ResultSet resuet =ps.executeQuery();
-	         
-	            	while(resuet.next())
+				 ps=con.prepareStatement("select * from Pet "); //  SQl query - Select everthing from Pet table
+				  ResultSet resuet =ps.executeQuery();
+	            	while(resuet.next())       
 	            		{
 	            		 Pet aMember= new Pet(resuet.getString("petname"),resuet.getString("breed"),resuet.getString("color"),resuet.getInt("petid"));
-	        	    aPet.add(aMember);
+	        	    aPet.add(aMember);  // Adding to the arraylist
 	           }
-					
-	        	     HttpSession session = request.getSession();
-
-
-
-								session.setAttribute("aPet", aPet);
-
-					           
-	        	    
-						 RequestDispatcher rdf = getServletContext().getRequestDispatcher("/WEB-INF/views/WelcomeAdmin.jsp");
-					rdf.include(request, response);
-		
+					 HttpSession session = request.getSession(); // Creating Http Session
+	        	     session.setAttribute("aPet", aPet);
+                     RequestDispatcher rdf = getServletContext().getRequestDispatcher("/WEB-INF/views/WelcomeAdmin.jsp");
+					rdf.include(request, response);         // Redirecting to Welcome Admin page
 			}
-
-catch (SQLException e)
+catch (SQLException e)      // Catch SQl Exception
 {
 e.printStackTrace();
 
 throw new ServletException("DB Connection problem.");
 }finally{
 try {
-ps.close();
-} catch (SQLException e) { e.printStackTrace();
-
+ps.close();    // Close DB Connection
+} catch (SQLException e) { 
+    e.printStackTrace();
 }
 }
-		}
+}
 	}
 }
 
