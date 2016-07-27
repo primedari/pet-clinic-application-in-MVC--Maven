2917544a8d97bbe6ea5c900e65e5a1be857917c9
primedari <priyanka.medari@gmail.com>
2016-07-26 20:09:39 -0400

    Update ControllerAdminServlet.java

--- src/main/java/com/pri/admin/servlet/ControllerAdminServlet.java
+++ src/main/java/com/pri/admin/servlet/ControllerAdminServlet.java
@@ -1,31 +1,17 @@
 package com.pri.admin.servlet;
 
-import java.io.IOException
-
-;
-
-
+import java.io.IOException ;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
-
 import com.pri.dao.PetDao;
-//import com.pri.servlet.util.Pet;
-
-
-
-
 
 @WebServlet (urlPatterns = {"/Insert","/Hurry"})
 public class ControllerAdminServlet extends HttpServlet {
@@ -34,29 +20,20 @@ public class ControllerAdminServlet extends HttpServlet {
 	{
 		dao= new PetDao();
 	}
-	
-	
  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		
-
 	String uri = request.getRequestURI();
-
-			
-		
-	
-		 if(uri.endsWith("Insert"))
+	 if(uri.endsWith("Insert"))             // Insert Values for Pet
 		{
 			String petname = request.getParameter("petname");
 			String breed = request.getParameter("breed");
 			String color= request.getParameter("color");
 			String petid = request.getParameter("petid");
-			
 			String errorMsg = null;
 			
 			if(petname  == null || petname.equals("")){
 				errorMsg = "Petname can't be null or empty.";
-			}
+			}                                                       /* Validation for fields*/
 			if(breed == null || breed.equals("")){
 				errorMsg = "Breed can't be null or empty.";
 			}
@@ -66,38 +43,30 @@ public class ControllerAdminServlet extends HttpServlet {
 			if(petid == null || petid.equals("")){
 				errorMsg = "Petid can't be null or empty.";
 			}
-			
-			
-			if(errorMsg != null){
+			if(errorMsg != null){                         //Redirect to Login Page for any errors
 				RequestDispatcher rd = getServletContext().getRequestDispatcher("/WEB-INF/views/Login.jsp");
 				PrintWriter out= response.getWriter();
 				out.println("<font color=red>"+errorMsg+"</font>");
 				rd.include(request, response);
 			}
 			else{
-				
-			Connection con = (Connection) getServletContext().getAttribute("DBConnection");
+			Connection con = (Connection) getServletContext().getAttribute("DBConnection"); // Database Connection
 			PreparedStatement ps = null;
-			try {
+			try {                                   // Insert values into Pet Table
 				ps = con.prepareStatement("insert into Pet(petname,breed,color,petid) values (?,?,?,?)");
 				ps.setString(1, petname);
 				ps.setString(2, breed);
 				ps.setString(3, color);
 				ps.setInt(4, Integer.parseInt(petid));
 				ps.execute();
-				System.out.println("inserted");
-				 
-				
-				
-				
-				//forward to login page to login
+			
+				//Forward to Login page 
 				RequestDispatcher rd = getServletContext().getRequestDispatcher("/WEB-INF/views/WelcomeAdmin.jsp");
 				PrintWriter out= response.getWriter();
 				out.println("<font color=green>Inserted in Pet successful, please login below.</font>");
 				rd.include(request, response);
 			} catch (SQLException e) {
 				e.printStackTrace();
-				
 				throw new ServletException("DB Connection problem.");
 			}finally{
 				try {
@@ -110,40 +79,36 @@ public class ControllerAdminServlet extends HttpServlet {
 		}
 	}
 		
-		else  if(uri.endsWith("Hurry"))
+	else  if(uri.endsWith("Hurry")) // Delete data from Pet table
 		{
 			int petid = Integer.parseInt(request.getParameter("petid"));
 			Connection con = (Connection) getServletContext().getAttribute("DBConnection");
 			PreparedStatement ps = null;
 			try {
-				
 				ps = con.prepareStatement("delete from Pet where petid=?");
 				ps.setInt(1, petid);
 				System.out.println(petid);
 				ps.executeUpdate();
-				System.out.println("Deleted !!!");
 				RequestDispatcher view12 = request.getRequestDispatcher("/WEB-INF/views/WelcomeAdmin.jsp");
-				
-		         PrintWriter out= response.getWriter();
+			    PrintWriter out= response.getWriter();
 				out.println("<font color=red>Deletion successful.Please Click on View</font>");
 				view12.include(request, response);
-			}catch (SQLException e)
+			}
+			catch (SQLException e)
 			{
 				e.printStackTrace();
 				
 				throw new ServletException("DB Connection problem.");
 			}finally{
 				try {
-					ps.close();
+					ps.close();    //Close DB Connection
 				} catch (SQLException e) { e.printStackTrace();
 					
 				}
 			}
 			
 		}
-	
-	
-}
+	}
 }
 
 
