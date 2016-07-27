10e82463c882af48ba57d5f19464b048d6718c34
primedari <priyanka.medari@gmail.com>
2016-07-26 20:20:38 -0400

    Update DogOwnerDao.java

--- src/main/java/com/pri/dao/DogOwnerDao.java
+++ src/main/java/com/pri/dao/DogOwnerDao.java
@@ -1,31 +1,19 @@
 package com.pri.dao;
 
-import java.sql.Connection
-;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-//import java.sql.SQLException;
-
 import com.pri.servlet.util.DBConnectionManager;
 
-
-
-public class DogOwnerDao {
+public class DogOwnerDao {       // DAO class for Owner
 	
 	Connection con;
 	PreparedStatement ps = null;
 	ResultSet rs = null;
-	
 	DBConnectionManager connection = new DBConnectionManager();
-	
-	
-	
 	
 	public DogOwnerDao() {
 		con = connection.getConnection();
-	}
-
-	
-
+	             }
 	}
 
