680cdc3601a417e8bfdb33ea008f78d2be7c06d3
primedari <priyanka.medari@gmail.com>
2016-07-26 20:21:42 -0400

    Update PetDao.java

--- src/main/java/com/pri/dao/PetDao.java
+++ src/main/java/com/pri/dao/PetDao.java
@@ -3,18 +3,14 @@ package com.pri.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
 import com.pri.servlet.util.DBConnectionManager;
 
-public class PetDao {
+public class PetDao {              //DAO class for Pet
 	Connection con;
 	PreparedStatement ps = null;
 	ResultSet rs = null;
-	
 	DBConnectionManager connection = new DBConnectionManager();
 	
-	
-
 	public PetDao() {
 		con = connection.getConnection();
 	}
